508e5f54496c05ccbcb5612d8b709d1b54cabdb6
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-04-29 14:53:35 -0300

    Links funcionando

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,13 +3,19 @@ export default function Contact() {
     <div id='contact' className='card border-top rounded shadow p-3 mb-5 bg-body rounded container'>
       <div className='row'>
         <div className='col'>
-          <p>Ícone do Gmail</p>
+          <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+            <img id="linkGoogle" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" alt="link do gmail" />
+          </a>
         </div>
         <div className='col'>
-          <p>Ícone do LinkedIn</p>
+        <a href="https://www.linkedin.com/in/joaopauloveloso/"  target="_blank" rel="noopener noreferrer">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" alt="link do linkedin" />
+        </a>
         </div>
         <div className='col'>
-          <p>Ícone do Github</p>
+        <a href="https://github.com/jpvsouza"  target="_blank" rel="noopener noreferrer">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="link do github" />
+        </a>
         </div>
       </div>
     </div>
