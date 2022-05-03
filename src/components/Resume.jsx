8e7c0895eb87c72098a385de635e1e9f01686a05
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-05-03 10:49:19 -0300

    Foto de perfil

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ export default function Resume() {
   <div id="resume" class="card border-dark mb-3">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" class="img-fluid rounded-start" alt="..."/>
+        <img id="perfil" src="https://lh3.googleusercontent.com/kl__0v8doMdBc5AnDwKIAQ1nXS9aW34fipFgnCLij-REQnDk526XS3G4arA5JVVa3my7L3meDbVzZTokXd2Iym7uhSDLQlGUGETQm_5xfiPW17S_w8PrHMfX73KMSLXAbQ6eK3GFVu5QW9Ll7L0NTmiPzYcwcZd42ZQceTKdstNPSfLJulGk5eEcDUIDNNhFRWcpTWz2tl1PQRUDYCZclG6z7symVPjgQwke6adyGBWQfdck5bBU0CM6MrbM-hkqBWCkgEpRilWdibwMnTG6NXzjqdhTw-IHY05RPVMeFmdckFhqfEw6oIvJ1ScZr3oDe0tZh6jyNp7bMcMHTzopwi1AyBvd6H43n9bRPL6M29TMOwckjEZ0UZSDeSH4mr4JcbmElZm6alUlEfoZxXvAtqZZ2SZw-dhikn3rBcq-Rz-AUnINhFZ1X0Mk_uORrECNZjybniK84oMRXwxcNMXa_EVP1j-NWAhDsYuKzHuC4tjh0oCMo-RFCGlRQTZ55K7hcAvQS9-XQ27BT1c3RXdVMGg94Q7yGte1kCqBV1AMd3aclVqmWzbEGu0HM_bySVHLUGrZnBgSujk8DyBsCRAnBSCdI2mjKDdI92AF7Wx2Ctq19TsthW6w_JJ5MgFgtFve5Yta-cv6NL1aHhAAvEGIIzwpiXFZbJce-xal6pW9k0u7AoDqVVEtaftBIy1o1dwviGUZa0qxrieYMTSDDxACoVXWJOv4ZBRH1Ep5lO4Hn5oLuBCjAoQ92oXxjUpiJQM=w600-h601-no?authuser=0" class="img-fluid" alt="..."/>
       </div>
       <div class="col-md-8">
         <div class="card-body">
