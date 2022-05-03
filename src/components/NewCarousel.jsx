1cfd40eb814941c9e2620196bbf2a11137105068
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-05-03 16:46:38 -0300

    Adicionado os links do carrossel

--- src/components/NewCarousel.jsx
+++ src/components/NewCarousel.jsx
@@ -3,38 +3,44 @@ import Carousel from 'react-bootstrap/Carousel';
 export default function NewCarousel() {
     return (<Carousel>
         <Carousel.Item>
+      <a href='https://jp-recipes.herokuapp.com/' rel='noreferrer' target='_blank'>
           <img
             className="d-block w-100"
             src="https://www.serebii.net/art/th/4.png"
             alt="First slide"
           />
+      </a>
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Recipes App</h3>
+            <p>Projeto de aplicativo de receitas chato de maaaaaais. Feito em grupo e consumindo duas APIs diferentes</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
+          <a href='https://jp-starwars.herokuapp.com/' rel='noreferrer' target='_blank'>
           <img
             className="d-block w-100"
             src="https://www.serebii.net/art/th/5.png"
             alt="Second slide"
           />
+          </a>
       
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Startwars Planets</h3>
+            <p>Aplicação que consome dados de uma API de planetas Starwars.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
+          <a  href='https://jp-store.herokuapp.com/' rel='noreferrer' target='_blank'>
           <img
             className="d-block w-100"
             src="https://www.serebii.net/art/th/6.png"
             alt="Third slide"
           />
+          </a>
       
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h3>Web Store</h3>
+            <p>Projeto que consomo API do mercado livre.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>);
